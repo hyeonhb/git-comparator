fdf225a9e84dc88ac2bcaf1586cbe7b3fbc42fb6
Hojung Kim <tora4094@inha.edu>
2024-06-06 13:53:58 +0900

    feature: Display 클래스 구현 및 Main 파일 추가

--- src/Display.py
+++ src/Display.py
@@ -1,2 +1,25 @@
+import pygame as pg
 class Display:
-    pass
+    def __init__(self, width, height, fps, name):
+        pg.init()
+        self.screen_width = width
+        self.screen_height = height
+        self.fps = fps
+        self.screen = pg.display.set_mode((self.screen_width, self.screen_height))
+        pg.display.set_caption(name)
+        self.clock = pg.time.Clock()        
+        pass
+
+    def start(self, controller):
+        try:
+            while True:
+                for event in pg.event.get():
+                    if event.type == pg.QUIT:
+                        return
+                    self.screen.fill((255, 255, 255))
+                    pg.display.flip()
+                    self.clock.tick(self.fps)
+        finally:
+            pg.quit()        
+
+        
---
+++ src/Main.py
@@ -0,0 +1,4 @@
+from Display import Display
+
+display = Display(1024, 768, 60, "SwamRobot Display")
+display.start("")
