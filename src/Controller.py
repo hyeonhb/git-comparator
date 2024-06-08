9c3c204745420328290dba48656b980a3458c4aa
tora4094 <161473029+tora4094@users.noreply.github.com>
2024-06-08 12:52:07 +0900

    feature: Controller 클래스에 메서드 및 생성자 추가
    - 생성자 추가: commander_list 초기화
    - 메소드 (add_commander, remove_commander) 추가

--- src/Controller.py
+++ src/Controller.py
@@ -1,2 +1,9 @@
 class Controller:
-    pass
+    def __init__(self, commander_list):
+        self.commander_list = commander_list
+
+    def add_commander(self, commander):
+        self.commander_list.append(commander)
+
+    def remove_commander(self, commander):
+        self.commander_list.remove(commander)
