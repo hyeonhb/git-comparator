654e452fca314200aefb9e0f7d46dd1fbbd5814e
tora4094 <161473029+tora4094@users.noreply.github.com>
2024-06-08 10:17:55 +0900

    feature: ICommander 클래스에 메서드 및 생성자 추가
    - 생성자 추가: swarm_list 초기화
    - Swarm 관련 메서드 (add_swarm, remove_swarm) 추가
    - Commander 관련 메서드 (commander_init, commander_start, commander_pause, commander_play, commander_stop) 추가

--- src/ICommander.py
+++ src/ICommander.py
@@ -1,2 +1,20 @@
 class ICommander:
-    pass
+    def __init__(self, swarm_list):
+        self.swarm_list = swarm_list
+
+    def add_swarm(self, swarm):
+        self.swarm_list.append(swarm)
+
+    def remove_swarm(self, swarm):
+        self.swarm_list.remove(swarm)
+
+    def commander_init(self):
+        pass
+    def commander_start(self):
+        pass
+    def commander_pause(self):
+        pass
+    def commander_play(self):
+        pass
+    def commander_stop(self):
+        pass
