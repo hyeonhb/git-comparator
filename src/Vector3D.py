0e29b4728b878e2b5473ccd0e0229abb5bd0322f
cwpark <cwpark1217@gmail.com>
2024-06-10 03:49:30 +0900

    feature: Vector3D 구현

--- src/Vector3D.py
+++ src/Vector3D.py
@@ -1,2 +1,30 @@
 class Vector3D:
-    pass
+    def __init__(self, x=0.0, y=0.0, z=0.0):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __add__(self, other):
+        return Vector3D(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __sub__(self, other):
+        return Vector3D(self.x - other.x, self.y - other.y, self.z - other.z)
+
+    def __mul__(self, scalar):
+        return Vector3D(self.x * scalar, self.y * scalar, self.z * scalar)
+
+    def __truediv__(self, scalar):
+        return Vector3D(self.x / scalar, self.y / scalar, self.z / scalar)
+
+    def __repr__(self):
+        return f"Vector3D({self.x}, {self.y}, {self.z})"
+
+    def lerp(self, other, t):
+        if not 0.0 <= t <= 1.0:
+            raise ValueError("The interpolation factor t must be between 0.0 and 1.0")
+        
+        return Vector3D(
+            self.x + (other.x - self.x) * t,
+            self.y + (other.y - self.y) * t,
+            self.z + (other.z - self.z) * t
+        )
