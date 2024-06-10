7be4a9c562a2bdf87e629db84ec38bc1a0b08bb0
cwpark <cwpark1217@gmail.com>
2024-06-11 08:32:00 +0900

    feature: Display 임시 구현

--- src/Display.py
+++ src/Display.py
@@ -1,25 +1,45 @@
-import pygame as pg
+import pygame
+import time
+
 class Display:
-    def __init__(self, width, height, fps, name):
-        pg.init()
+    def __init__(self, engine, width, height, fps=60, name=""):
+        pygame.init()
         self.screen_width = width
         self.screen_height = height
         self.fps = fps
-        self.screen = pg.display.set_mode((self.screen_width, self.screen_height))
-        pg.display.set_caption(name)
-        self.clock = pg.time.Clock()        
-        pass
+        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+        pygame.display.set_caption(name)
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.engine = engine
 
-    def start(self, controller):
+        self.tag_images = {
+            'Drone': self.load_image('.\\resource\\free-icon-camera-drone-5524149.png'),
+            'RobotCar': self.load_image('.\\resource\\free-icon-camera-drone-5524149.png'),
+        }
+
+    def load_image(self, image_path):
+        return pygame.image.load(image_path)
+
+    def start(self):
         try:
-            while True:
-                for event in pg.event.get():
-                    if event.type == pg.QUIT:
+            while self.running:
+                for event in pygame.event.get():
+                    start_time = time.time()
+                    if event.type == pygame.QUIT:
                         return
-                    self.screen.fill((255, 255, 255))
-                    pg.display.flip()
-                    self.clock.tick(self.fps)
-        finally:
-            pg.quit()        
+                self.screen.fill((255, 255, 255))
 
-        
+                object_list = self.engine.get_object_list()
+                for object in object_list:
+                    image = pygame.transform.scale(self.tag_images[object.config.tag], (object.config.width, object.config.depth))
+                    self.screen.blit(image, (object.position.x, object.position.z))
+
+                pygame.display.flip()
+                self.clock.tick(self.fps)
+                print(time.time() - start_time)
+        finally:
+            pygame.quit()
+
+    def stop(self):
+        self.running = False
