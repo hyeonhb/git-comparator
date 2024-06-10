6c23a6727a08b48377fc644fe0d8321727d5bf92
cwpark <cwpark1217@gmail.com>
2024-06-11 08:32:56 +0900

    feature: Drone, RobotCar에 Config 추가

--- src/Drone.py
+++ src/Drone.py
@@ -1,6 +1,8 @@
+from IVirtualObject import VirtualObjectConfig
 from Vector3D import Vector3D
 from VirtualSwarmRobot import VirtualSwarmRobot
 
 class Drone(VirtualSwarmRobot):
     def __init__(self, engine, name="", position=Vector3D(), orientation=Vector3D()):
         super().__init__(engine, name, position, orientation)
+        self.config = VirtualObjectConfig(width=30, height=3, depth=30, tag="Drone")
--- src/RobotCar.py
+++ src/RobotCar.py
@@ -1,9 +1,11 @@
+from IVirtualObject import VirtualObjectConfig
 from Vector3D import Vector3D
 from VirtualSwarmRobot import VirtualSwarmRobot
 
 class RobotCar(VirtualSwarmRobot):
     def __init__(self, engine, name="RobotCar", position=Vector3D(), orientation=Vector3D()):
         super().__init__(engine, name, position, orientation)
+        self.config = VirtualObjectConfig(width=10, height=3, depth=10, tag="RobotCar")
 
     def move(self, velocity):
         velocity.y = 0.0 #y축으로 이동할 수 없음(비행 X)
