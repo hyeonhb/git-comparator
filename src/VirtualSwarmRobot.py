bdeb6b2fb78f6513a7af44945c4a7b32996fc54d
cwpark <cwpark1217@gmail.com>
2024-06-10 22:50:31 +0900

    feature: VirtualSwarmRobot 구현

--- src/VirtualSwarmRobot.py
+++ src/VirtualSwarmRobot.py
@@ -1,6 +1,23 @@
 from IVirtualObject import IVirtualObject
+import math
 from SwarmRobot import SwarmRobot
+import time
+from Vector3D import Vector3D
 
 class VirtualSwarmRobot(IVirtualObject, SwarmRobot):
-    def __init__(self, engine, name=""):
-        super().__init__(engine, name)
+    def __init__(self, engine, name="", position=Vector3D(), orientation=Vector3D()):
+        IVirtualObject.__init__(self, engine, name)
+        SwarmRobot.__init__(self, position, orientation)
+
+    def update_orientation(self):
+        if self.angular_velocity != 0.0:
+            self.orientation = self.orientation + (self.angular_velocity * self.engine.delta_time)
+            self.orientation = self.orientation % (2 * math.pi) #방향은 2PI의 주기를 가짐
+
+    def update_position(self):
+        if self.velocity != 0.0:
+            self.position = self.position + (self.velocity * self.engine.delta_time)
+
+    def update(self):
+        self.update_orientation()
+        self.update_position()
