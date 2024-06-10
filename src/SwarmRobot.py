39e142190322a4d6845374c53a219028dcffa116
cwpark <cwpark1217@gmail.com>
2024-06-10 23:01:39 +0900

    feature: SwarmRobot 구현

--- src/SwarmRobot.py
+++ src/SwarmRobot.py
@@ -1,5 +1,18 @@
 from ISwarm import ISwarm
+from Vector3D import Vector3D
 
 class SwarmRobot(ISwarm):
-    def __init__ (self, position, orientation):
+    def __init__ (self, position=Vector3D(), orientation=Vector3D()):
         super().__init__(position, orientation)
+        self.velocity = Vector3D()
+        self.angular_velocity = Vector3D()
+
+    def rotate(self, angular_velocity):
+        self.angular_velocity = angular_velocity
+
+    def move(self, velocity):
+        self.velocity = velocity
+
+    def stop(self):
+        self.angular_velocity = Vector3D()
+        self.velocity = Vector3D()
