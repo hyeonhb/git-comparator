3709007936b565f343c3f4b1a959fcf06a76fd92
cwpark <cwpark1217@gmail.com>
2024-06-10 23:05:05 +0900

    refactor: Drone class의 테스트 코드 제거

--- src/Drone.py
+++ src/Drone.py
@@ -4,7 +4,3 @@ from VirtualSwarmRobot import VirtualSwarmRobot
 class Drone(VirtualSwarmRobot):
     def __init__(self, engine, name="", position=Vector3D(), orientation=Vector3D()):
         super().__init__(engine, name, position, orientation)
-
-    def update(self):
-        print(f"Test Drone[{self.position}]")
-        return super().update()
