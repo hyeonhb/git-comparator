4ae312a5c3d4ffa7757475063113edd8af27da0e
cwpark <cwpark1217@gmail.com>
2024-06-10 23:03:02 +0900

    refactor: Drone, RobotCar class가 잘못 상속받는 문제 해결

--- src/Drone.py
+++ src/Drone.py
@@ -1,5 +1,10 @@
+from Vector3D import Vector3D
 from VirtualSwarmRobot import VirtualSwarmRobot
 
 class Drone(VirtualSwarmRobot):
-    def __init__(self, engine, name=""):
-        super().__init__(engine, name)
+    def __init__(self, engine, name="", position=Vector3D(), orientation=Vector3D()):
+        super().__init__(engine, name, position, orientation)
+
+    def update(self):
+        print(f"Test Drone[{self.position}]")
+        return super().update()
--- src/RobotCar.py
+++ src/RobotCar.py
@@ -1,5 +1,10 @@
+from Vector3D import Vector3D
 from VirtualSwarmRobot import VirtualSwarmRobot
 
 class RobotCar(VirtualSwarmRobot):
-    def __init__(self, engine, name=""):
-        super().__init__(engine, name)
+    def __init__(self, engine, name="RobotCar", position=Vector3D(), orientation=Vector3D()):
+        super().__init__(engine, name, position, orientation)
+
+    def move(self, velocity):
+        velocity.y = 0.0 #y축으로 이동할 수 없음(비행 X)
+        return super().move(velocity)
