6465a3b642084c4f1de123e0513a545fa2314b3a
hyeonhb <hyeonnhb@gmail.com>
2024-06-11 21:24:15 +0900

    featrue: 더미용 Ant class를 사용해서 2차원 환경에서의 Commander 로직 구현 완료

--- src/CommanderAnt.py
+++ src/CommanderAnt.py
@@ -1,3 +1,67 @@
 from ICommander import ICommander
+from DummytAnt import DummyAnt
+import math
+import pygame
+
+# Flocking params
+MAX_VELOCITY = 1.2
+PERCEPTION_RADIUS = 50
+SEPARATION_DISTANCE = 30
+
 class CommanderAnt(ICommander):
-    pass
+    def __init__(self, swarm_list):
+        self.swarm_list = [
+            [DummyAnt(), DummyAnt(), DummyAnt(), DummyAnt()],
+            [DummyAnt(), DummyAnt()],
+            [DummyAnt(), DummyAnt(), DummyAnt(), DummyAnt(), DummyAnt(), DummyAnt(), DummyAnt()],
+        ]
+
+    def get_next_position_list(self):
+        result = []
+
+        for swarm in self.swarm_list:
+            swram_position_list = []
+            for current_boid in swarm:
+                for other in swarm:
+                    if other == current_boid:
+                        continue
+                    
+                    average_velocity = pygame.Vector2(0, 0)
+                    average_position = pygame.Vector2(0, 0)
+                    average_separation = pygame.Vector2(0, 0)
+                    neighbors_counter = 0
+
+                    distance = self.get_distance_difference(current_boid, other)
+                    if distance < PERCEPTION_RADIUS:
+                        neighbors_counter += 1
+
+                        average_velocity += other.velocity
+                        average_position += other.position
+
+                    if distance < SEPARATION_DISTANCE:
+                        diff = self.position - other.position
+                        diff.scale_to_length(1 / distance)
+
+                        average_separation += diff
+
+                if neighbors_counter > 0:
+                    average_velocity /= neighbors_counter
+                    average_position /= neighbors_counter
+                    average_separation /= neighbors_counter
+
+                current_boid.velocity += average_velocity * 0.08
+                current_boid.velocity += average_position * 0.05
+                current_boid.velocity -= average_separation * 0.12
+                current_boid.velocity.scale_to_length(MAX_VELOCITY)
+                
+                swram_position_list.append(current_boid.velocity)
+            
+            result.append(swram_position_list)
+        return result
+
+    def get_distance_difference(self, current_boid, other):
+        # 유클리디안 거리 (Euclidean Distance)
+        # 거리차이 값에는 음수 개념이 없기 때문에 각 좌표 차이를 제곱해서 더해주고, 최종적으로는 제곱근 값을 리턴해준다.
+        position_diff = (current_boid.position[0] - other.position[0]) ** 2 + (current_boid.position[1] - other.position[1]) ** 2
+        return math.sqrt(position_diff)
+
