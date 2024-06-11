f5cb459492b847d377b96839b05d1fc8e6a69cda
hyeonhb <hyeonnhb@gmail.com>
2024-06-11 21:24:50 +0900

    test: 2차원 CommanderAnt의 결과값 출력 부분 Main에서 호출

--- src/Main.py
+++ src/Main.py
@@ -1,9 +1,14 @@
 from Display import Display
 from VirtualSimEngine import VirtualSimEngine
+from CommanderAnt import CommanderAnt
 
 if __name__ == '__main__':
     engine = VirtualSimEngine()
     engine.start()
+
+    commander = CommanderAnt([])
+    next_position = commander.get_next_position_list()
+    print('!!! next_position:', next_position)
 
     display = Display(engine, width=1024, height=768, name="SwamRobot Display")
     display.start()
