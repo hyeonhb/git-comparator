a54c3b96153139e945b9d3465a16dab2045e22e4
hyeonhb <hyeonnhb@gmail.com>
2024-06-13 01:40:22 +0900

    test: 테스트 코드 제거

--- src/Main.py
+++ src/Main.py
@@ -6,11 +6,6 @@ if __name__ == '__main__':
     engine = VirtualSimEngine()
     engine.start()
 
-    commander = CommanderAnt([])
-    commander.commander_start()
-    next_velocity = commander.set_next_velocity()
-    print('!!! next_velocity:', next_velocity)
-
     display = Display(engine, width=1024, height=768, name="SwamRobot Display")
     display.start()
 
