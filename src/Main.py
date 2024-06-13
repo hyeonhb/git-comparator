5264ae0efeaa7bd4f034cde12725be7f01dc6a3c
cwpark <cwpark1217@gmail.com>
2024-06-13 18:59:49 +0900

    featrue: Main 코드 작성

--- src/Main.py
+++ src/Main.py
@@ -1,5 +1,11 @@
 from Display import Display
+from Drone import Drone
+import math
+from Vector3D import Vector3D
 from VirtualSimEngine import VirtualSimEngine
+from Controller import *
+from Swarm import Swarm
+from CommanderBird import CommanderBird
 from CommanderAnt import CommanderAnt
 
 if __name__ == '__main__':
@@ -9,4 +15,10 @@ if __name__ == '__main__':
     display = Display(engine, width=1024, height=768, name="SwamRobot Display")
     display.start()
 
+    app = Controller(engine, display)
+    app.add_commander(CommanderAnt(engine))
+    app.add_commander(CommanderAnt(engine))
+    app.start()
+
+    display.stop()
     engine.stop()
