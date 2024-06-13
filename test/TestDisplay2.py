c966ccded3b026d5d6b2009713e0224ddd6c2532
Hojung Kim <tora4094@inha.edu>
2024-06-13 14:49:53 +0900

    test: Controller를 사용하도록 변경

--- test/TestDisplay2.py
+++ test/TestDisplay2.py
@@ -12,6 +12,7 @@ from Vector3D import Vector3D
 from VirtualSimEngine import VirtualSimEngine
 from Controller import *
 from Swarm import Swarm
+from CommanderAnt import CommanderAnt
 
 if __name__ == '__main__':
     engine = VirtualSimEngine()
@@ -21,6 +22,7 @@ if __name__ == '__main__':
     drone2 = Drone(engine, "Drone2", position=Vector3D(300, 0, 300))
     drone3 = Drone(engine, "Drone2", position=Vector3D(500, 0, 500))
     drone4 = Drone(engine, "Drone3", position=Vector3D(600, 0, 200))
+    drone5 = Drone(engine, "Drone4", position=Vector3D(500, 760, 200))
 
     print(drone1.position)
     print(drone2.position)
@@ -30,17 +32,23 @@ if __name__ == '__main__':
 
     drone3.rotate(Vector3D(0.0, - (math.pi), 0.0))
     drone4.move(Vector3D(0.0, 5.0, 0.0))
+    drone5.move(Vector3D(0.0, -5.0, 0.0))
 
     swarm1 = Swarm()
     swarm1.add_robot(drone1)
     swarm1.add_robot(drone2)
     swarm1.add_robot(drone3)
+    swarm1.add_robot(drone4)
+    swarm1.add_robot(drone5)
 
-    # root = tk.Tk()
-    # app = VirtualSwarmPanel(root, swarm1, engine)
-    # root.mainloop()
+    commander_ant = CommanderAnt(engine)
+    commander_ant.add_swarm(swarm1)
 
     display = Display(engine, width=1024, height=768, name="SwamRobot Display")
     display.start()
+
+    app = Controller(engine, display)
+    app.add_commander(commander_ant)
+    app.start()
 
     engine.stop()
