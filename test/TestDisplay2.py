9070cb4c7f41f6755124a407b1dfbae271670ed5
Hojung Kim <tora4094@inha.edu>
2024-06-13 20:07:42 +0900

    test: Swarm 두개의 테스트 구문 작성

--- test/TestDisplay2.py
+++ test/TestDisplay2.py
@@ -13,6 +13,7 @@ from VirtualSimEngine import VirtualSimEngine
 from Controller import *
 from Swarm import Swarm
 from CommanderAnt import CommanderAnt
+from CommanderBird import CommanderBird
 
 if __name__ == '__main__':
     engine = VirtualSimEngine()
@@ -20,9 +21,9 @@ if __name__ == '__main__':
 
     drone1 = Drone(engine, "Drone1")
     drone2 = Drone(engine, "Drone2", position=Vector3D(300, 0, 300))
-    drone3 = Drone(engine, "Drone2", position=Vector3D(500, 0, 500))
-    drone4 = Drone(engine, "Drone3", position=Vector3D(600, 0, 200))
-    drone5 = Drone(engine, "Drone4", position=Vector3D(500, 760, 200))
+    drone3 = Drone(engine, "Drone3", position=Vector3D(500, 0, 500))
+    drone4 = Drone(engine, "Drone4", position=Vector3D(600, 0, 200))
+    drone5 = Drone(engine, "Drone5", position=Vector3D(500, 760, 500))
 
     print(drone1.position)
     print(drone2.position)
@@ -41,13 +42,29 @@ if __name__ == '__main__':
     swarm1.add_robot(drone4)
     swarm1.add_robot(drone5)
 
+    commander_bird = CommanderBird(engine)
+    commander_bird.add_swarm(swarm1)
+    
+    robotcar1 = RobotCar(engine, "Robotcar1")
+    robotcar2 = RobotCar(engine, "Robotcar2", position=Vector3D(300, 0, 300))
+    robotcar3 = RobotCar(engine, "Robotcar3", position=Vector3D(300, 0, 300))
+
+    robotcar1.move(Vector3D(100, 0, 100))
+    robotcar2.move(Vector3D(-100, 0, -100))
+
+    swarm2 = Swarm()
+    swarm2.add_robot(robotcar1)
+    swarm2.add_robot(robotcar2)
+    swarm2.add_robot(robotcar3)
+
     commander_ant = CommanderAnt(engine)
-    commander_ant.add_swarm(swarm1)
+    commander_ant.add_swarm(swarm2)
 
     display = Display(engine, width=1024, height=768, name="SwamRobot Display")
     display.start()
 
     app = Controller(engine, display)
+    app.add_commander(commander_bird)
     app.add_commander(commander_ant)
     app.start()
 
